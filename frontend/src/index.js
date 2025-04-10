a2fcd9e0b17acce4a72c9bff137044951f679d7a
Kamil Kostka <kamilkostka6@gmail.com>
2025-04-10 19:23:46 +0200

    Fixed unnecessary import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,6 @@ import './index.css'; // Global styles
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './context/AuthContext';
-import './firebase';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
